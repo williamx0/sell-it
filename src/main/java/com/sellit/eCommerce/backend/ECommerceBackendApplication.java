7a8d449816e8d89d04f71af9e8b481f34e58ae91
William ☕️ <113069637+williamx0@users.noreply.github.com>
2023-08-15 23:53:30 +0100

    Initial commit

---
+++ src/main/java/com/sellit/eCommerce/backend/ECommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sellit.eCommerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sellit/eCommerce/backend/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellit.eCommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
